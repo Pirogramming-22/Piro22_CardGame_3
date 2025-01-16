d1023d1f48376b266d2e8dedbdcc88358bff286b
junhkchoi <leisure1566@gmail.com>
2025-01-17 02:49:50 +0900

    변경사항 저장

--- game/views.py
+++ game/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from users.models import User
 
 # Create your views here.
-<<<<<<< HEAD
 def gameInfo1(request):
     return render(request, 'gameInfo/gameInfo1.html')
 
@@ -11,7 +10,7 @@ def gameInfo2(request):
 
 def gameInfo3(request):
     return render(request, 'gameInfo/gameInfo3.html')
-=======
+
 def create_game(request):
     return render(request, 'smk_gameStart/smk_Attack.html')
 
@@ -24,4 +23,3 @@ def game_list(request):
 #         'users':users,
 #     }
 #     return render(request, 'game/rank.html', context)
->>>>>>> f1d2a0bc48523e989157dfa638fc4a9a7cc0d74a
