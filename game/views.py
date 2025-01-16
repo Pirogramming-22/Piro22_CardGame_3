f7d52adb8dccbf21ffb65996179f29db5815bbe8
dietken1 <dietken1@ajou.ac.kr>
2025-01-17 00:45:28 +0900

    중간 구현 저장

--- game/views.py
+++ game/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from users.models import User
 
 # Create your views here.
+def rank(request):
+    users = User.objects.order_by('-score')[:3]
+    context = {
+        'users':users,
+    }
+    return render(request, 'game/rank.html', context)
