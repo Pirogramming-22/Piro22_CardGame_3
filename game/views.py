5e991df52b49f4c584ddb9bed469c26634595142
03minseok <iris15977@gmail.com>
2025-01-17 00:44:02 +0900

    url

--- game/views.py
+++ game/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def create_game(request):
+    return render(request, 'smk_gameStart/smk_Attack.html')
+
+def game_list(request):
+    return render(request, 'game/cms.html')
+
+def rankings(request):
+    return render(request, 'game/jwj.html')
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+  return render(request, 'auth/main.html')
+
+def login_view(request):
+  return render(request, 'auth/login.html')
+
+def signup_view(request):
+  return render(request, 'auth/signup.html')
+  
