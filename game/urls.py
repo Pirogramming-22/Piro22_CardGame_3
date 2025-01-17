a0fa5f802712a9ee59e8ec9f11110432d8e9095f
junhkchoi <leisure1566@gmail.com>
2025-01-17 14:48:09 +0900

    게임 정보 연결

--- game/urls.py
+++ game/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('attackSave/', views.attackSave, name='attackSave'),  # 게임 생성 (공격)
     # path('<int:game_id>/', views.game_detail, name='game_detail'),  # 특정 게임 상세 보기
     path('list/<int:user_id>/', views.game_list, name='game_list'),  # 게임 리스트 보기
-    path('gameInfo1/', views.gameInfo1),
-    path('gameInfo2/', views.gameInfo2),
-    path('gameInfo3/', views.gameInfo3),
+    path('gameInfo1/', views.gameInfo1, name='gameInfo1'), #게임 진행중일때 정보
+    path('gameInfo2/', views.gameInfo2), # 카운터 어텍 상황일때 정보
+    path('gameInfo3/', views.gameInfo3), # 게임 결과가 나왔을때 정보
     path('respond/<int:game_id>/', views.respond_game, name='respond_game'),  # 게임에 반격
     path('respondSave/<int:game_id>', views.respondSave, name='respondSave'),
     path('rankings/', views.rankings, name='rankings'),  # 랭킹 보기
