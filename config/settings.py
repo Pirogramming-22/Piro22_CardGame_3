b79d586846780e7be3aadfe9090e8b2666e14cb3
yeeeww <yeangsshi@ewhain.net>
2025-01-17 19:48:45 +0900

    머지

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -90,9 +91,20 @@ AUTH_PASSWORD_VALIDATORS = [
 SITE_ID = 1
 
 SOCIALACCOUNT_LOGIN_ON_GET = True
-LOGIN_REDIRECT_URL = 'main'
-ACCOUNT_LOGOUT_REDIRECT_URL = 'index'
-ACCOUNT_LOGOUT_ON_GET = True 
+LOGIN_REDIRECT_URL = '/'
+ACCOUNT_LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_LOGOUT_ON_GET = True
+SOCIALACCOUNT_PROVIDERS = {
+    'kakao': {
+        'SCOPE': [
+            'profile_nickname',   #sns플랫폼에 따라 달라지는 부분분
+
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
 
 LANGUAGE_CODE = 'ko'
 
